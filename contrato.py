f895f6ffe6c8dcb3f19768f30b4d04376778bb8b
Matheus Beck <matheussbeck@outlook.com>
2024-09-10 22:49:28 -0300

    first commmit

--- contrato.py
+++ contrato.py
@@ -1,7 +1,14 @@
 from datetime import datetime
 from typing import Tuple
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, validate_call
+from enum import Enum
 
+class ProdutoEnum(str,Enum)
+    
 class vendas(BaseModel):
-    email:
+    email: Emailstr
+    data: datetime
+    valor: float
+    quantidade: int
+    produto: str
